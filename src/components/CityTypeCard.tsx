f4dcfc7838310a2f9cd53615cf1e78fb5c057cf2
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-06-02 18:32:25 -0300

    feat: city cards component for travel types

---
+++ src/components/CityTypeCard.tsx
@@ -0,0 +1,55 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import ReactCountryFlag from "react-country-flag";
+
+interface CityTypeCardProps {
+  image: string;
+  city: string;
+  country: string;
+  countryInicials: string;
+  continent: string;
+}
+
+export function CityTypeCard({ image, city, country, countryInicials, continent }: CityTypeCardProps) {
+  return (
+    <Flex w="550px">
+      <Image  
+        src={image} 
+        w="280px"
+        h="220px"
+        objectFit="cover"
+        borderLeftRadius="6px"
+      />
+      <Box 
+        borderRight="4px" 
+        borderRightWidth="1px" 
+        borderRightColor="yellow.200"
+        borderRightRadius="6px" 
+        borderTopWidth="1px" 
+        borderTopColor="yellow.200"
+        borderBottomWidth="1px"
+        borderBottomColor="yellow.200"
+      >
+        <Flex direction="column" justify="space-between" p="10" h="100%">
+          <Text fontSize="3xl" lineHeight="8" fontWeight="semibold" fontFamily="Barlow" mb="4">{city}</Text>
+          <Flex w="52" justify="space-between" align="center">
+            <Box>
+              <Text fontSize="lg" fontWeight="medium" color="gray.400" fontFamily="Barlow">{country}</Text>
+              <Text fontSize="sm" fontWeight="medium" color="gray.300" fontFamily="Barlow">{continent}</Text>
+            </Box>
+            <ReactCountryFlag
+              countryCode={countryInicials}
+              svg
+              cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/"
+              cdnSuffix="svg"
+              style={{
+                width: "2rem",
+                height: "2rem",
+                borderRadius: "50%",
+              }}
+            />
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+}
