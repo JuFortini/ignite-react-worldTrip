fd033b1443d10dc4c1f42c5454ee516a1ec3d7f8
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-06-02 11:51:13 -0300

    feat: travel types page

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -5,19 +5,19 @@ import { TypeCard } from "./TypeCard";
 export function TravelTypes() {
   return (
     <HStack justify="space-between" w="80%" mx="44" mb="20" mt="28">
-      <TypeCard href="#" src="images/cocktail.png">
+      <TypeCard href="/travel-types/night-life" src="images/cocktail.png">
         vida noturna
       </TypeCard>
-      <TypeCard href="#" src="images/beach.png">
+      <TypeCard href="/travel-types/beach" src="images/beach.png">
         praia
       </TypeCard>
-      <TypeCard href="#" src="images/modern.png">
+      <TypeCard href="/travel-types/modern" src="images/modern.png">
         moderno
       </TypeCard>
-      <TypeCard href="#" src="images/classic.png">
+      <TypeCard href="/travel-types/classic" src="images/classic.png">
         clássico
       </TypeCard>
-      <TypeCard href="#" src="images/world.png">
+      <TypeCard href="/travel-types" src="images/world.png">
         e mais ...
       </TypeCard>
     </HStack>
---
+++ src/pages/travel-types/index.tsx
@@ -0,0 +1,31 @@
+import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { TypesBanner } from "../../components/Banners/TypesBanner";
+import { Header } from "../../components/Header";
+import { TypeCard } from "../../components/TravelTypes/TypeCard";
+
+export default function TravelTypes() {
+  return (
+    <Flex direction="column" w="100%" align="center" justify="center" mb="28">
+      <Header />
+      <TypesBanner />
+      <SimpleGrid
+        mt="32"
+        px="32"
+        w="100%"
+        templateColumns="repeat(auto-fit, minmax(230px, 230px))"
+        justifyContent="space-between"
+        spacingY="12"
+      >
+        <TypeCard href="/travel-types/adventure" src="/images/adventure.png">aventura</TypeCard>
+        <TypeCard href="/travel-types/classic" src="/images/classic.png">clássico</TypeCard>
+        <TypeCard href="/travel-types/food" src="/images/food.png">comida</TypeCard>
+        <TypeCard href="/travel-types/cruise" src="/images/cruise.png">cruzeiro</TypeCard>
+        <TypeCard href="/travel-types/modern" src="/images/modern.png">moderno</TypeCard>
+        <TypeCard href="/travel-types/snow" src="/images/cold.png">neve</TypeCard>
+        <TypeCard href="/travel-types/beach" src="/images/beach.png">praia</TypeCard>
+        <TypeCard href="/travel-types/romantic" src="/images/romantic.png">romântico</TypeCard>
+        <TypeCard href="/travel-types/night-life" src="/images/cocktail.png">vida noturna</TypeCard>
+      </SimpleGrid>
+    </Flex>
+  );
+}
