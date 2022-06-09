5cddac05c1091565b6d1658017c06c5d50b415d5
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-06-09 10:25:39 -0300

    Vercel deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+let baseURL:string;
+
 export const api = axios.create({
-  baseURL: "/api"
+  baseURL: baseURL
+})
+
+api.interceptors.request.use((config) => {
+  if (process.env.NODE_ENV === "development") {
+    config.baseURL = "http://localhost:3000/api";
+  } else {
+    config.baseURL = "/api";
+  }
+
+  return config;
 })
